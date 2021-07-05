99f9795338992f939e4a0e51d009ba6643c47e0f
Peter Borrow <borrow.peter83@gmail.com>
2021-07-05 08:03:38 +0100

    REFACTOR: Re implemented hooks componenet for usestate

--- src/hooks.js
+++ src/hooks.js
@@ -3,20 +3,7 @@ import {useState} from 'react';
 
 
 
-const MultiplyByOne = () => {
-    const [count, setCount] = useState(0)
-    return(
-        <>
-            <p>You Clicked {count} times:</p>
-            <button onClick={() =>setCount(count +1)}>
-                Click Me
-            </button>
-         
-        </>
-    );
-}
 
-export default MultiplyByOne;
     
 
 
