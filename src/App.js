3c0e6716c2e03c051061fb276d532cf73b71db58
Peter Borrow <borrow.peter83@gmail.com>
2021-07-11 17:02:23 +0100

    FEATURE: Added new items arrray

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from "react"
 
 
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2.30pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeting at School',
+            day: 'Mar 17th at 17.30pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Bootcamp',
+            day: 'Jul 12th at 9.00am',
+            reminder: true,
+        },
+        {
+            id: 4,
+            text: 'Shopping',
+            day: 'Aug 26th at 16.15',
+            reminder: false,
+        }
+    ]
+)
 
   return (
     <div>
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   )
     
